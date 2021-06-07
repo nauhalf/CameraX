c223391e1d880934ddf39d007038fe9b81a75222
Dzaky <dzakyrahmanto@gmail.com>
2021-06-07 11:31:30 +0700

    add function takesnapshot to gallery change output directory to every function take add support to below android q (takesnapshot) disable takepicture for a while

--- app/src/main/java/com/nauhalf/camerax/MainViewModel.kt
+++ app/src/main/java/com/nauhalf/camerax/MainViewModel.kt
@@ -6,20 +6,20 @@ import androidx.lifecycle.ViewModel
 import id.dipay.camerax.Selector
 
 class MainViewModel : ViewModel() {
+
     private val _cameraSelector = MutableLiveData(Selector.BACK)
+    val cameraSelector: LiveData<Selector>
+        get() = _cameraSelector
+    fun setCameraSelector(value: Selector) {
+        _cameraSelector.value = value
+    }
+
+
     private val _timer = MutableLiveData(0)
     val timer: LiveData<Int>
         get() = _timer
-
     fun setTimer(value: Int){
         _timer.value = value
-    }
-
-    val cameraSelector: LiveData<Selector>
-        get() = _cameraSelector
-
-    fun setCameraSelector(value: Selector) {
-        _cameraSelector.value = value
     }
 
 }
